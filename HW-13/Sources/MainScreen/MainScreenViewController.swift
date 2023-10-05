8a2ff7802ef050ac2c91b48602b1b7a7c257a6e0
Gabitosz <gabriel.zdravkovici313@gmail.com>
2023-10-05 22:09:36 +0300

    Add settings options and customizing

--- HW-13/Sources/MainScreen/MainScreenViewController.swift
+++ HW-13/Sources/MainScreen/MainScreenViewController.swift
@@ -7,15 +7,10 @@
 
 import UIKit
 
-struct SettingsOption {
-    let title: String
-    let icon: UIImage?
-    let iconBackgroundColor: UIColor
-    let handler: (() -> Void)
-}
-
 class MainScreenViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var enableAirplane = false
+    var enableVPN = false
     
     // MARK: Outlets
     
@@ -25,7 +20,7 @@ class MainScreenViewController: UIViewController, UITableViewDelegate, UITableVi
         return table
     }()
     
-    var models = [SettingsOption]()
+    var models = [SettingsSection]()
     // MARK: Lifecycle
     
     override func viewDidLoad() {
@@ -39,18 +34,81 @@ class MainScreenViewController: UIViewController, UITableViewDelegate, UITableVi
         setupLayout()
         configure()
         
-       
+        
     }
     
     func configure() {
-        self.models = Array(0...100).compactMap({
-            SettingsOption(title: "Item \($0)", icon: UIImage(systemName: "house"), iconBackgroundColor: .purple) {
+        
+        models.append(SettingsSection(options: [
+            SettingsOption(title: "Авиарежим", icon: UIImage(named: "airplane"), iconBackgroundColor: UIColor(red: 240 / 255, green: 154 / 255, blue: 54 / 255, alpha: 1)){
+                
+            },
+            
+            SettingsOption(title: "Wi-Fi", icon: UIImage(named: "wifi"), iconBackgroundColor: UIColor(red: 52 / 255, green: 120 / 255, blue: 246 / 255, alpha: 1)){
+                
+            },
+            
+            SettingsOption(title: "Bluetooth", icon: UIImage(named: "bluetooth"), iconBackgroundColor: UIColor(red: 52 / 255, green: 120 / 255, blue: 246 / 255, alpha: 1)){
+                
+            },
+            
+            SettingsOption(title: "Сотовая связь", icon: UIImage(named: "cellular"), iconBackgroundColor: UIColor(red: 101 / 255, green: 196 / 255, blue: 102 / 255, alpha: 1)){
+                
+            },
+            
+            SettingsOption(title: "Режим модема", icon: UIImage(named: "modem"), iconBackgroundColor: UIColor(red: 101 / 255, green: 196 / 255, blue: 102 / 255, alpha: 1)){
+                
+            },
+            
+            SettingsOption(title: "VPN", icon: UIImage(named: "vpn"), iconBackgroundColor: UIColor(red: 52 / 255, green: 120 / 255, blue: 246 / 255, alpha: 1)){
                 
             }
-        })
+        ]))
+        
+        models.append(SettingsSection(options: [
+            SettingsOption(title: "Уведомления", icon: UIImage(named: "notification"), iconBackgroundColor: UIColor(red: 235 / 255, green: 77 / 255, blue: 60 / 255, alpha: 1)){
+                
+            },
+            
+            SettingsOption(title: "Звуки, тактильные сигналы", icon: UIImage(named: "sound"), iconBackgroundColor: UIColor(red: 234 / 255, green: 67 / 255, blue: 90 / 255, alpha: 1)){
+                
+            },
+            
+            SettingsOption(title: "Не беспокоить", icon: UIImage(named: "moon"), iconBackgroundColor: UIColor(red: 87 / 255, green: 86 / 255, blue: 206 / 255, alpha: 1)){
+                
+            },
+            
+            SettingsOption(title: "Экранное время", icon: UIImage(named: "hourglass"), iconBackgroundColor: UIColor(red: 87 / 255, green: 86 / 255, blue: 206 / 255, alpha: 1)){
+                
+            }
+        ]))
+        
+        models.append(SettingsSection(options: [
+            SettingsOption(title: "Основные", icon: UIImage(named: "settings"), iconBackgroundColor: UIColor(red: 142 / 255, green: 142 / 255, blue: 146 / 255, alpha: 1)){
+                
+            },
+            
+            SettingsOption(title: "Пункт управления", icon: UIImage(named: "switch"), iconBackgroundColor: UIColor(red: 142 / 255, green: 142 / 255, blue: 146 / 255, alpha: 1)){
+                
+            },
+            
+            SettingsOption(title: "Экран и яркость", icon: UIImage(named: "screenAndBright"), iconBackgroundColor: UIColor(red: 52 / 255, green: 120 / 255, blue: 246 / 255, alpha: 1)){
+                
+            },
+            
+            SettingsOption(title: "Экран «Домой»", icon: UIImage(named: "home"), iconBackgroundColor: UIColor(red: 52 / 255, green: 120 / 255, blue: 246 / 255, alpha: 1)){
+                
+            },
+            
+            SettingsOption(title: "Универсальный доступ", icon: UIImage(named: "universalAccess"), iconBackgroundColor: UIColor(red: 52 / 255, green: 120 / 255, blue: 246 / 255, alpha: 1)){
+                
+            },
+            
+        ]))
+        
         
     }
-
+    
     // MARK: Setup
     
     private func setupHierarchy() {
@@ -63,20 +121,63 @@ class MainScreenViewController: UIViewController, UITableViewDelegate, UITableVi
     }
     
     // MARK: - Actions
-   
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return models.count
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models[section].options.count
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let model = models[indexPath.row]
+        let model = models[indexPath.section].options[indexPath.row]
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.identifier, for: indexPath) as? SettingsTableViewCell else {
             return UITableViewCell()
+        }
+        
+        if model.title == "Авиарежим" {
+            let switchView = UISwitch(frame: .zero)
+            switchView.setOn(false, animated: true)
+            switchView.addTarget(self, action: #selector(toggleAirplane), for: .valueChanged)
+            cell.accessoryView = switchView
+        }  else if model.title == "VPN" {
+            let switchView = UISwitch(frame: .zero)
+            switchView.setOn(false, animated: true)
+            switchView.addTarget(self, action: #selector(toggleVPN), for: .valueChanged)
+            cell.accessoryView = switchView
+        }   else {
+            cell.accessoryView = nil
         }
         
         cell.configure(model: model)
         return cell
     }
+    
+    @objc func toggleAirplane(_ sender: UISwitch) {
+        self.enableAirplane = sender.isOn
+        print(enableAirplane)
+    }
+    
+    @objc func toggleVPN(_ sender: UISwitch) {
+        self.enableVPN = sender.isOn
+        print(enableVPN)
+    } 
+}
+
+// Setting
+
+struct SettingsOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler: (() -> Void)
+}
+
+// Section
+
+struct SettingsSection {
+    let options: [SettingsOption]
 }
 
